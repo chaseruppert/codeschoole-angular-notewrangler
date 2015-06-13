86709186fce20ae9280da38b3286ae037b633c7d
Chase Ruppert <chase.ruppert@live.com>
2015-06-13 07:20:33 -0700

    latest progress on angular tuts

--- javascript/directives/nw-card.js
+++ javascript/directives/nw-card.js
@@ -3,9 +3,9 @@ angular.module('NoteWrangler')
         return {
             restrict: 'E',
             templateUrl: 'templates/directives/nw-card.html',
-            controller: function($scope) {
-                $scope.header = 'Note Title';
-                $scope.description = 'A lovely note description.';
+            scope: {
+                header: "=",
+                description: "="
             }
         };
 }]);
